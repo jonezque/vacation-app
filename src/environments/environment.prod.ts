19a4d44b57d209ff993f6f24f0045fa7cbe38f8b
Artem Brilev <artem.brilev@skuvault.com>
2019-09-07 14:26:20 +0300

    Init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDn4YVZygIgs8uVTkm55mUi-cdrosH5SZ4',
+    authDomain: 'vacation-app-ffd17.firebaseapp.com',
+    databaseURL: 'https://vacation-app-ffd17.firebaseio.com',
+    projectId: 'vacation-app-ffd17',
+    storageBucket: 'vacation-app-ffd17.appspot.com',
+    messagingSenderId: '968912702025',
+    appId: '1:968912702025:web:199da3f9bd14c17f698d98'
+  }
 };
